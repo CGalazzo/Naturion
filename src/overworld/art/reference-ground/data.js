bffed89be50acc72c6666e4e8c1b0bbb19530b0f
Caio Galazzo <caio.galazzo@gmail.com>
2026-07-31 20:15:04 -0300

    fix: reunir dados compactados da arte do Bosque

---
+++ src/overworld/art/reference-ground/data.js
@@ -0,0 +1,11 @@
+import c0 from './chunk-00.js';
+import c1 from './chunk-01.js';
+import c2 from './chunk-02.js';
+import c3 from './chunk-03.js';
+import c4 from './chunk-04.js';
+import c5 from './chunk-05.js';
+import c6a from './chunk-06a.js';
+import c6b from './chunk-06b.js';
+
+export const REFERENCE_GROUND_COMPRESSED_BASE64 = [c0,c1,c2,c3,c4,c5,c6a,c6b].join('');
+export const REFERENCE_GROUND_FORMAT = Object.freeze({ width: 216, height: 156, colors: 128 });
