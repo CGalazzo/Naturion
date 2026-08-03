7ba42af693ebc9cf433caadfff8a2db0c875da71
Caio Galazzo <caio.galazzo@gmail.com>
2026-08-03 20:10:56 -0300

    Carregar chegada do Totem e regras de reinício

--- src/overworld/echo-main.js
+++ src/overworld/echo-main.js
@@ -3,3 +3,4 @@ import "./idle/background-fix.js?v=1";
 import "./idle/phase1.js?v=1";
 import "./idle/treadmill.js?v=4";
 import "./idle/battle-encounter-polish.js?v=1";
+import "./idle/totem-run-reset.js?v=1";
