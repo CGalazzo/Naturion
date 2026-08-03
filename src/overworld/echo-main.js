f6974e5e8fc2b534e6491d2539c601a91fe95f1a
Caio Galazzo <caio.galazzo@gmail.com>
2026-08-03 20:00:13 -0300

    Carregar melhorias de batalha e encontros idle

--- src/overworld/echo-main.js
+++ src/overworld/echo-main.js
@@ -2,3 +2,4 @@ import "./idle/phase1-compat.js?v=1";
 import "./idle/background-fix.js?v=1";
 import "./idle/phase1.js?v=1";
 import "./idle/treadmill.js?v=4";
+import "./idle/battle-encounter-polish.js?v=1";
