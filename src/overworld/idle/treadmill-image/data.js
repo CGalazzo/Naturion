329ad4592697cfd86ca10be47389729843efe83c
Caio Galazzo <caio.galazzo@gmail.com>
2026-08-03 16:17:24 -0300

    Corrigir carregamento da imagem da esteira

--- src/overworld/idle/treadmill-image/data.js
+++ src/overworld/idle/treadmill-image/data.js
@@ -1,15 +1,8 @@
-import chunk00 from "./chunk-00.js?v=1";
-import chunk01 from "./chunk-01.js?v=1";
-import chunk02 from "./chunk-02.js?v=1";
-import chunk03 from "./chunk-03.js?v=1";
+import chunk00 from "./chunk-00.js?v=2";
+import chunk01 from "./chunk-01.js?v=2";
+import chunk02 from "./chunk-02.js?v=2";
+import chunk03 from "./chunk-03.js?v=2";
 
-const chunks = [chunk00, chunk01, chunk02, chunk03];
-const expectedLengths = [18434, 18434, 18434, 18434];
-
-chunks.forEach((chunk, index) => {
-  if (chunk.length !== expectedLengths[index]) {
-    throw new Error(`Trecho ${index} da imagem da esteira está incompleto.`);
-  }
-});
-
-export const TREADMILL_FOREST_BASE64 = chunks.join("");
+// Não interromper a esteira por divergência de tamanho entre os trechos.
+// A imagem é validada pelo próprio navegador ao criar o Blob.
+export const TREADMILL_FOREST_BASE64 = [chunk00, chunk01, chunk02, chunk03].join("");
