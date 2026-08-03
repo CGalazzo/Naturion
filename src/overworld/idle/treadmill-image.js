2f4813a3967a041abb4031cac25620fdb5631967
Caio Galazzo <caio.galazzo@gmail.com>
2026-08-03 16:17:38 -0300

    Tornar decodificação da esteira tolerante

--- src/overworld/idle/treadmill-image.js
+++ src/overworld/idle/treadmill-image.js
@@ -1,13 +1,13 @@
-import { TREADMILL_FOREST_BASE64 } from "./treadmill-image/data.js?v=1";
+import { TREADMILL_FOREST_BASE64 } from "./treadmill-image/data.js?v=2";
 
 let treadmillImageUrl = "";
 
 const decodeBase64 = (base64) => {
-  const binary = atob(base64);
+  const clean = String(base64 || "").replace(/\s+/g, "");
+  if (!clean) throw new Error("Imagem da esteira vazia.");
+  const binary = atob(clean);
   const bytes = new Uint8Array(binary.length);
-  for (let index = 0; index < binary.length; index += 1) {
-    bytes[index] = binary.charCodeAt(index);
-  }
+  for (let index = 0; index < binary.length; index += 1) bytes[index] = binary.charCodeAt(index);
   return bytes;
 };
 
