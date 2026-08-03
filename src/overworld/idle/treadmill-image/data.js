cace598698530f6e798e8ca7d18878a2be07657d
Caio Galazzo <caio.galazzo@gmail.com>
2026-08-03 19:41:37 -0300

    Montar imagem validada da esteira

--- src/overworld/idle/treadmill-image/data.js
+++ src/overworld/idle/treadmill-image/data.js
@@ -1,8 +1,18 @@
-import chunk00 from "./chunk-00.js?v=2";
-import chunk01 from "./chunk-01.js?v=2";
-import chunk02 from "./chunk-02.js?v=2";
-import chunk03 from "./chunk-03.js?v=2";
+import chunk00a from "./chunk-00-a.js?v=1";
+import chunk00b from "./chunk-00-b.js?v=1";
+import chunk00c from "./chunk-00-c.js?v=1";
+import chunk00d from "./chunk-00-d.js?v=1";
+import chunk01 from "./chunk-01.js?v=3";
+import chunk02 from "./chunk-02.js?v=3";
+import chunk03 from "./chunk-03.js?v=3";
 
-// Não interromper a esteira por divergência de tamanho entre os trechos.
-// A imagem é validada pelo próprio navegador ao criar o Blob.
-export const TREADMILL_FOREST_BASE64 = [chunk00, chunk01, chunk02, chunk03].join("");
+const chunks = [chunk00a, chunk00b, chunk00c, chunk00d, chunk01, chunk02, chunk03];
+const expectedLengths = [4608, 4608, 4608, 4610, 18434, 18434, 18434];
+
+chunks.forEach((chunk, index) => {
+  if (chunk.length !== expectedLengths[index]) {
+    throw new Error(`Trecho ${index} da imagem da esteira está incompleto.`);
+  }
+});
+
+export const TREADMILL_FOREST_BASE64 = chunks.join("");
