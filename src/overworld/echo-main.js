e63430a00d9150a33511632da36ca699b8eb7f68
Caio Galazzo <caio.galazzo@gmail.com>
2026-08-10 16:18:00 -0300

    Carrega arte exata do Guardião

--- src/overworld/echo-main.js
+++ src/overworld/echo-main.js
@@ -5,3 +5,4 @@ import "./idle/treadmill.js?v=6";
 import "./idle/central-sanctuary.js?v=2";
 import "./idle/central-guardian-scene.js?v=1";
 import "./idle/central-guardian-art.js?v=1";
+import "./idle/central-guardian-art-v2.js?v=1";
