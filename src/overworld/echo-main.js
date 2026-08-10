350668926e6a0e80b8e92678a4cd39465bceed80
Caio Galazzo <caio.galazzo@gmail.com>
2026-08-10 15:50:43 -0300

    Carrega visual do novo Guardião

--- src/overworld/echo-main.js
+++ src/overworld/echo-main.js
@@ -4,3 +4,4 @@ import "./idle/phase1.js?v=8";
 import "./idle/treadmill.js?v=6";
 import "./idle/central-sanctuary.js?v=2";
 import "./idle/central-guardian-scene.js?v=1";
+import "./idle/central-guardian-art.js?v=1";
